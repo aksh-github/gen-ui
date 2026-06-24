c526a71d3360fc0b1a39af09b2383351ee4074a7
Akshay B <beatle02@gmail.com>
2026-06-24 13:31:13 +0530

    REMOVE ALL from master branch

--- backend/src/core/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class Settings(BaseModel):
-    stream_chunk_size: int = 8
-
-
-settings = Settings()
--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-import { h } from "@vdom-lib";
-import { Chat } from "./components/Chat";
-
-export function App() {
-  return <Chat />;
-}
